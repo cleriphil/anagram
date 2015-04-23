358f2c738cd2836dc0b593ef22db1244d1ac536a
Guest User <student@epicodus.com>
2015-04-23 16:55:03 -0700

    Add and pass first spec

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,18 @@
-
-
 class String
   define_method(:anagram) do |list|
+    list = list.split(",")
     word = self
+    results = Array.new()
+    letters = word.split("")
+    first_letter = letters[0]
+    list.each() do |guess|
+      num_of_letters = guess.length()
+      num_of_letters.times() do |time|
+      current_letter = letters[time]
+      present = guess.include?(current_letter)
+      results.push(present)
+      end
+    end
+  results
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,2 +1,13 @@
 require('rspec')
 require('anagram')
+
+#splits self and returns true if the first letter is in each word in the argument
+
+describe('String#anagram') do
+  it("splits self and returns true if the first letter is in each word in the argument") do
+  expect("box".anagram("xob")).to(eq([true, true, true]))
+  end
+  it("returns") do
+  expect("box".anagram("xob, oxb")).to(eq([true, true, true, true, true, true]))
+end
+end
